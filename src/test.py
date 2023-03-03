cb3f7baefb3f42088695dc6216e2d367e1d10c35
Abhishek Sinha <abhishek.sinha132@gmail.com>
2023-03-03 11:59:49 +0530

    added test file in src folder

---
+++ src/test.py
@@ -0,0 +1,5 @@
+""" This is being used as testig if pyhton is installed or not. """
+
+print("hello World")
+
+""" if this executes without any error. It means python is installed in your machine """
